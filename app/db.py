a598e5675b24a1fe83910cd7094c24405df18691
Adam <adam@vps143856.vps.ovh.ca>
2017-10-12 01:36:31 -0400

    Able to connect to postgres from python with psycopg2

---
+++ app/db.py
@@ -0,0 +1,22 @@
+import psycopg2
+class DB:
+	def __init__(self):
+			self.conn = psycopg2.connect("dbname='postgres' user='postgres' host='db' password='pass'")
+			
+			print('Connected to database')
+		
+	
+	def createTable(self):
+		cur = self.conn.cursor()
+		cur.execute("""CREATE TABLE Posts (\
+						id serial PRIMARY KEY,\
+						link varchar,\
+						_like integer,\
+						_time timestamp DEFAULT now())""")
+		cur.execute("""CREATE TABLE tags (\
+						id serial PRIMARY KEY,\
+						tag varchar,\
+						_time timestamp DEFAULT now())""")
+		self.conn.commit()
+		
+db = DB()
